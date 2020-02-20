5df0924ceb1ee6a478ef2d47c706d687120c3e92
tslight <tslight@pm.com>
2020-02-20 14:19:35 +0000

    deal with untagged images too

--- azacr.sh
+++ azacr.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-# infinite loop to get simple yes or no user input
+
 ask() {
     local question="$1"
 
     while :; do
-	# -e for readline bindings
-	# -r to not mangle backslashes
-	# -n 1 for execution without return
 	read -rep "$question " ans
 	case $ans in
 	    [Yy][Ee][Ss]|[Yy])
@@ -25,18 +22,27 @@ ask() {
     done
 }
 
+###############################################################################
+#                                OLD MANIFESTS                                #
+###############################################################################
 get_old_manifests() {
     local registry="$1" repository="$2" older_than="$3"
 
-    echo "Getting manifests from $registry/$repository older than $older_than..."
+    echo "Getting manifests from $registry/$repository last updated before $older_than..."
     IFS=$'\n'
     OLD_MANIFESTS=($(az acr repository show-manifests \
 			--name "$registry" \
 			--repository "$repository" \
 			--orderby time_asc \
 			--output tsv \
-			--query "[?timestamp < '$older_than'].[digest, timestamp]"))
+			--detail \
+			--query "[?lastUpdateTime < '$older_than'].[digest, tags, createdTime, lastUpdateTime]"))
     unset IFS
+
+    if [[ "${#OLD_MANIFESTS[@]}" -lt 1 ]]; then
+	echo "No manifests last updated before $older_than in $registry/$repository."
+	exit 0
+    fi
 }
 
 show_old_manifests() {
@@ -46,9 +52,10 @@ show_old_manifests() {
     if ask "Would you like to view them?"; then
 	for manifest in "${OLD_MANIFESTS[@]}"; do
 	    echo "$manifest"
-	done
+	done | "$PAGER"
     fi
 }
+
 
 delete_old_manifests() {
     local registry="$1" image="$2" older_than="$3"
@@ -64,6 +71,58 @@ delete_old_manifests() {
     fi
 }
 
+###############################################################################
+#                          UNTAGGED (ORPHANED) IMAGES                         #
+###############################################################################
+get_untagged_images() {
+    local registry="$1" repository="$2"
+
+    echo "Getting untagged (orphaned) images from $registry/$repository..."
+    IFS=$'\n'
+    UNTAGGED_IMAGES=($(az acr repository show-manifests \
+			  --name "$registry" \
+			  --repository "$repository" \
+			  --orderby time_asc \
+			  --output tsv \
+			  --detail \
+			  --query "[?tags[0]==null].[digest, tags, createdTime, lastUpdateTime]"))
+    unset IFS
+
+    if [[ "${#UNTAGGED_IMAGES[@]}" -lt 1 ]]; then
+	echo "No untagged (orphaned) manifests in $registry/$repository."
+	exit 0
+    fi
+}
+
+show_untagged_images() {
+    local registry="$1" repository="$2"
+
+    echo "Found ${#UNTAGGED_IMAGES[@]} untagged (orphaned) images in $registry/$repository."
+    if ask "Would you like to view them?"; then
+	for image in "${UNTAGGED_IMAGES[@]}"; do
+	    echo "$image"
+	done | "$PAGER"
+    fi
+}
+
+delete_untagged_images() {
+    local registry="$1" image="$2"
+
+    if ask "Would you like to delete all untagged (orphaned) images in $registry/$image?"; then
+	for manifest in "${UNTAGGED_IMAGES[@]}"; do
+	    manifest="$(echo $manifest | awk '{print $1}')"
+	    az acr repository delete \
+	       --name "$registry" \
+	       --image "$image@$manifest" \
+	       --yes
+	done
+    fi
+}
+
 get_old_manifests "$@"
 show_old_manifests "$@"
 delete_old_manifests "$@"
+
+get_untagged_images "$@"
+show_untagged_images "$@"
+delete_untagged_images "$@"
