0836a5fc35bfeaf932c1f96dfed1e007646d8a02
tslight <tslight@pm.com>
2020-03-08 22:44:42 +0000

    blah

--- photo_org.py
+++ photo_org.py
@@ -1,84 +1,90 @@
+#!/usr/bin/env python
 import argparse
+import logging
 import os
 from datetime import datetime
 from pathlib import Path
-from PIL import Image
-from PIL.ExifTags import TAGS
+from PIL import Image, ExifTags
 
 
 def chkpath(path):
     """
-    Checks for valid directory path.
+    Checks if a path exists.
     """
     if os.path.exists(path):
-        if os.path.isdir(path):
-            return path
-        else:
-            msg = "{0} is not a directory.".format(path)
+        return os.path.abspath(path)
     else:
         msg = "{0} does not exist.".format(path)
-
-    raise argparse.ArgumentTypeError(msg)
+        raise argparse.ArgumentTypeError(msg)
 
 
 def getargs():
     """
     Return a list of valid arguments. If no argument is given, default to $PWD.
     """
-    parser = argparse.ArgumentParser(
-        description='Remove invalid characters from a given path.')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-i", "--interactive", action="store_true",
-                       help="prompt before renaming each path")
-    group.add_argument("-a", "--automate", action="store_true",
-                       help="rename each path without prompting")
-    group.add_argument("-t", "--dry_run", action="store_true",
-                       help="preform a dry run to see what would be renamed")
-    group.add_argument("-q", "--quiet", action="store_true",
-                       help="run silently")
-    group.add_argument("-f", "--find", action="store_true",
-                       help="print a list of invalid paths")
-    parser.add_argument("path", type=chkpath, nargs='?',
-                        default=".", help="a valid path")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path", type=chkpath, nargs="?", default=".", help="a valid path"
+    )
+    parser.add_argument(
+        "-v", "--verbosity", action="count", default=0, help="increase output verbosity"
+    )
     return parser.parse_args()
 
 
-def get_exiftags(path):
-    exiftags = {}
-
-    try:
-        img = Image.open(path)
-        if hasattr(img, '_getexif'):
-            exifdata = img._getexif()
-        else:
-            return
-
-        if exifdata and exifdata != 'None':
-            for tag, value in exifdata.items():
-                tagname = TAGS.get(tag)
-                exiftags[tagname] = value
-        else:
-            return
-
-    except IOError:
-        print("IOError accessing {}".format(path))
-
-    return exiftags
-
-
-def get_date(path):
-    exiftags = get_exiftags(path)
-    exifdate = "DateTimeOriginal"
-
-    if exiftags:
-        if exifdate in exiftags:
-            date = datetime.strptime(exiftags[exifdate], '%Y:%m:%d %H:%M:%S')
-            date = datetime.strftime(date, '%Y-%m-%d_%H-%M-%S')
-            return date
-        else:
-            return "NO " + exifdate + " TAG"
+def mklog(verbosity):
+    if verbosity > 1:
+        loglevel = logging.DEBUG
+    elif verbosity == 1:
+        loglevel = logging.INFO
     else:
-        return "NO EXIFTAGS"
+        loglevel = logging.WARNING
+
+    logging.basicConfig(
+        # filename='',
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=loglevel,
+        stream=sys.stdout,
+    )
+
+
+# def get_exiftags(path):
+#     exiftags = {}
+
+#     try:
+#         img = Image.open(path)
+#         if hasattr(img, "_getexif"):
+#             exifdata = img._getexif()
+#         else:
+#             return
+
+#         if exifdata and exifdata != "None":
+#             for tag, value in exifdata.items():
+#                 tagname = TAGS.get(tag)
+#                 exiftags[tagname] = value
+#         else:
+#             return
+
+#     except IOError:
+#         print("IOError accessing {}".format(path))
+
+#     return exiftags
+
+
+# def get_date(path):
+#     exiftags = get_exiftags(path)
+#     exifdate = "DateTimeOriginal"
+
+#     if exiftags:
+#         if exifdate in exiftags:
+#             date = datetime.strptime(exiftags[exifdate], "%Y:%m:%d %H:%M:%S")
+#             date = datetime.strftime(date, "%Y-%m-%d_%H-%M-%S")
+#             return date
+#         else:
+#             return "NO " + exifdate + " TAG"
+#     else:
+#         return "NO EXIFTAGS"
 
 
 def main():
@@ -106,14 +112,21 @@ def main():
     for ext in vidext:
         videos = videos + list(Path(path).rglob(ext))
 
-    for img in images:
-        date = get_date(img)
-        print("{} = {}".format(img, date))
+    # for img in images:
+    #     date = get_date(img)
+    #     print("{} = {}".format(img, date))
 
-    for vid in videos:
-        # https://stackoverflow.com/a/52492906
-        date = get_date(vid)
+    # for vid in videos:
+    #     # https://stackoverflow.com/a/52492906
+    #     date = get_date(vid)
+
+    for image in videos:
+        img = Image.open(image)
+        exif = dict(img.getexif())
+        for key, val in exif.items():
+            if key in ExifTags.TAGS and "DateTime" in ExifTags.TAGS[key]:
+                print(f"{ExifTags.TAGS[key]}:{repr(val)}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
