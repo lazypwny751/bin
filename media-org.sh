e7d6710813f467586ae60a78902780cda39f614d
tslight <tslight@pm.com>
2020-03-10 10:39:51 +0000

    add named params

--- media-org.sh
+++ media-org.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
-[ -n "$1" ] && SRC="$1" || SRC="$HOME/Dropbox/Camera Uploads"
-[ -n "$2" ] && DST="$2" || DST="$HOME/Dropbox/Pictures"
+while getopts ":s:d:t:h" opt; do
+    case $opt in
+	h) echo "$(basename "$0") [-s [SOURCE]] [-d [DESTINATION]] [-t [TAG]]"
+	   exit 0
+	   ;;
+	s) SRC="$OPTARG"
+	   ;;
+	d) DST="$OPTARG"
+	   ;;
+	t) TAG="$OPTARG"
+	   ;;
+	\?) echo "INVALID OPTION -$OPTARG" >&2
+	    ;;
+    esac
+done
+
+[ -z "$SRC" ] && SRC="$HOME/Dropbox/Camera Uploads"
+[ -z "$DST" ] && DST="$HOME/Dropbox/Pictures"
+[ -z "$TAG" ] && TAG="createdate"
 
 FILE_FMT="%Y%m%d.%H%M%S%%c.%%le"
 DIR_FMT="$DST/%Y/%m"
 
-# exiftool -quiet '-filename<contentcreatedate' -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
-# exiftool -quiet '-directory<contentcreatedate' -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
-exiftool -quiet '-filename<createdate' -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
-exiftool -quiet '-directory<createdate' -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
-# exiftool -quiet '-filename<datetimeoriginal' -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
-# exiftool -quiet '-directory<datetimeoriginal' -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
-# exiftool -quiet '-filename<filemodifydate' -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
-# exiftool -quiet '-directory<filemodifydate' -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
+exiftool "-filename<$TAG" -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
+exiftool "-directory<$TAG" -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
