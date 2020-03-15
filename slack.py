18004559c19771f3fadebb7b624f131e713bd88c
tslight <tslight@pm.com>
2020-03-15 00:16:08 +0000

    fix logging stuff

--- slack.py
+++ slack.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
 
-import requests
 import logging
+import sys
 from argparse import ArgumentParser
 from configparser import ConfigParser
 from os.path import expanduser
-from sys import exit, stdout
 from lib.py.slack import SlackClient
 
 
-def make_log(verbosity):
+def get_args():
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-c", "--channel", default="test-channel", help="send a slack command"
+    )
+    parser.add_argument(
+        "-g", "--get_messages", action="store_true", help="get messages from channel"
+    )
+    parser.add_argument(
+        "-l", "--limit", type=int, default=10, help="limit number of retrieved messages"
+    )
+    parser.add_argument("-p", "--post_message", help="a message to post to slack")
+    parser.add_argument("-s", "--send_command", nargs=2, help="send a slack command")
+    parser.add_argument("-t", "--token", help="pass in a slack token")
+    parser.add_argument("-u", "--user", default="Bot", help="send a slack command")
+    parser.add_argument("-v", action="count", default=0, help="increase verbosity")
+
+    return parser.parse_args()
+
+
+def mklog(verbosity):
+    """
+    Set root logging level based on number of -v's passed in at command line.
+    """
     if verbosity > 1:
         loglevel = logging.DEBUG
         logformat = "%(asctime)s %(threadName)s %(levelname)s %(message)s"
@@ -19,60 +42,38 @@ def make_log(verbosity):
     else:
         loglevel = logging.WARNING
         logformat = "%(levelname)s %(message)s"
-        logging.basicConfig(
-            # filename='',
-            format=logformat,
-            datefmt="%Y-%m-%d %H:%M:%S",
-            level=loglevel,
-            stream=stdout,
-        )
+
+    logging.basicConfig(
+        # filename='',
+        format=logformat,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=loglevel,
+        stream=sys.stdout,
+    )
 
 
 def get_token():
+    logging.info(
+        f"No token provided. Checking {expanduser('~')}/.slack.token instead..."
+    )
     try:
         config = ConfigParser()
         config.read(f'{expanduser("~")}/.slack.token')
         token = config["slack"]["token"]
     except Exception as error:
-        logging.error(f"Failed to retrieve token from ~/.slack.token. Aborting.")
-        exit(1)
+        logging.error(
+            f"Failed to retrieve token from {expanduser('~')}/.slack.token. Aborting."
+        )
+        sys.exit(1)
 
     if token:
+        logging.debug(f"Token: {token}")
         return token
 
-    logging.error(f"Failed to retrieve token from ~/.slack.token. Aborting.")
-    exit(1)
-
-
-def get_args():
-    parser = ArgumentParser()
-
-    parser.add_argument(
-        "-c", "--channel", default="test-channel", help="send a slack command",
+    logging.error(
+        f"Failed to retrieve token from {expanduser('~')}/.slack.token. Aborting."
     )
-    parser.add_argument(
-        "-g", "--get_messages", action="store_true", help="get messages from channel",
-    )
-    parser.add_argument(
-        "-l", "--limit", type=int, default=10, help="number of messages to retrieve",
-    )
-    parser.add_argument(
-        "-p", "--post_message", help="a message to post to slack",
-    )
-    parser.add_argument(
-        "-s", "--send_command", nargs=2, help="send a slack command",
-    )
-    parser.add_argument(
-        "-t", "--token", default=get_token(), nargs=2, help="send a slack command",
-    )
-    parser.add_argument(
-        "-u", "--user", default="Slack Bot", help="send a slack command",
-    )
-    parser.add_argument(
-        "-v", "--verbosity", action="count", default=0, help="increase output verbosity"
-    )
-
-    return parser.parse_args()
+    sys.exit(1)
 
 
 if __name__ == "__main__":
@@ -85,7 +86,11 @@ if __name__ == "__main__":
     Or pass it in as a command line option. It's up to you.
     """
     args = get_args()
-    make_log(args.verbosity)
+    mklog(args.v)
+
+    if args.token is None:
+        args.token = get_token()
+
     slack = SlackClient(args.token, args.channel, args.user)
 
     if args.post_message:
