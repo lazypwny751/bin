dae9c89b34e10555fc627143ef8dc13ff6bf5dfd
tslight <tslight@pm.com>
2020-04-01 20:36:51 +0100

    better formatting

--- diskpart.sh
+++ diskpart.sh
@@ -4,7 +4,10 @@
 [ -n "$1" ] && disk="$1" || { echo "$(basename "$0") [DISK] [LABEL]"; exit 1; }
 [ -n "$2" ] && label="$2" || { echo "$(basename "$0") [DISK] [LABEL]"; exit 1; }
 
-sgdisk --zap-all "$disk" && partprobe "$disk" && lsblk &&
-    parted --script "$disk" mklabel gpt &&
-    parted --script mkpart primary ext4 0% 100% &&
-    blockdev --getalignoff "$disk" && mkfs.ext4 -L "$label" "${disk}1" && lsblk
+sgdisk --zap-all "$disk" && \
+    partprobe "$disk" && \
+    parted --script "$disk" mklabel gpt && \
+    parted --script mkpart primary ext4 0% 100% && \
+    blockdev --getalignoff "$disk" && \
+    mkfs.ext4 -L "$label" "${disk}1" \
+    && lsblk -f
