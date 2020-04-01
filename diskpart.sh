42b8183a6232ea4e5c2537b082b12c44a846169c
tslight <tslight@pm.com>
2020-04-01 20:31:41 +0100

    do partitioning too

--- diskpart.sh
+++ diskpart.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 [ "$(id -u)" -ne 0 ] && { echo "This script must be run as root."; exit 1; }
-[ -n "$1" ] && disk="$1" || { echo "$(basename "$0") [DISK]"; exit 1; }
+[ -n "$1" ] && disk="$1" || { echo "$(basename "$0") [DISK] [LABEL]"; exit 1; }
+[ -n "$2" ] && label="$2" || { echo "$(basename "$0") [DISK] [LABEL]"; exit 1; }
 
-sgdisk --zap-all "$disk" && partprobe "$disk" &&
+sgdisk --zap-all "$disk" && partprobe "$disk" && lsblk &&
     parted --script "$disk" mklabel gpt mkpart primary ext4 0% 100% &&
-    blockdev --getalignoff "$disk"
+    blockdev --getalignoff "$disk" && mkfs.ext4 -L "$label" "$disk" && lsblk
