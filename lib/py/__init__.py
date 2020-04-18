ddaeb2a8ab1a54655c79ddbecb944007ba59d006
tslight <tslight@pm.com>
2020-04-19 00:18:48 +0100

    move cpager and cansi to their own repos

--- lib/py/__init__.py
+++ lib/py/__init__.py
@@ -1,5 +1,4 @@
-from .culor import addstr
 from .slack import SlackClient
 from .ftp_extras import ftp_rmr, ftp_mkdirp
 
-__all__ = ["addstr", "ftp_rmr", "ftp_mkdirp", "SlackClient"]
+__all__ = ["ftp_rmr", "ftp_mkdirp", "SlackClient"]
