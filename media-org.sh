7342e2ac9ea39e69a001d71dc4566785be7772da
tslight <tslight@pm.com>
2021-08-01 15:09:25 +0100

    update media org script

--- media-org.sh
+++ media-org.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 while getopts ":s:d:t:h" opt; do
     case $opt in
-	h) echo "$(basename "$0") [-s [SOURCE]] [-d [DESTINATION]] [-t [TAG]]"
-	   exit 0
-	   ;;
-	s) SRC="$OPTARG"
-	   ;;
-	d) DST="$OPTARG"
-	   ;;
-	t) TAG="$OPTARG"
-	   ;;
-	\?) echo "INVALID OPTION -$OPTARG" >&2
-	    ;;
+        h) echo "$(basename "$0") [-s [SOURCE]] [-d [DESTINATION]] [-t [TAGS]]"
+           exit 0
+           ;;
+        s) SRC="$OPTARG"
+           ;;
+        d) DST="$OPTARG"
+           ;;
+        t) TAGS="$OPTARG"
+           ;;
+        \?) echo "INVALID OPTION -$OPTARG" >&2
+            ;;
     esac
 done
 
 [ -z "$SRC" ] && SRC="$HOME/Dropbox/Camera Uploads"
 [ -z "$DST" ] && DST="$HOME/Dropbox/Pictures"
-[ -z "$TAG" ] && TAG="createdate"
+[ -z "$TAGS" ] && TAGS=(
+        "createdate"
+        "datetimeoriginal"
+        "filemodifydate"
+        "modifydate"
+    )
 
 FILE_FMT="%Y%m%d.%H%M%S%%c.%%le"
 DIR_FMT="$DST/%Y/%m"
 
-exiftool "-filename<$TAG" -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
-exiftool "-directory<$TAG" -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
+for tag in "${TAGS[@]}"; do
+    exiftool "-filename<$tag" -dateFormat "$FILE_FMT" -recurse -extension "*" "$SRC"
+    exiftool "-directory<$tag" -dateFormat "$DIR_FMT" -recurse -extension "*" "$SRC"
+done
