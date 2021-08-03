4bfc4cdf18196dc89b4ded2a6faab169b54fd1cd
tslight <tslight@pm.com>
2021-08-03 17:15:25 +0100

    make more robust

--- rename_from_exif.sh
+++ rename_from_exif.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 NC="$(tput sgr0)"
-GRN="$(tput setaf 2)"
+RED="$(tput setaf 1)"
 YEL="$(tput setaf 3)"
+MAG="$(tput setaf 5)"
 CYN="$(tput setaf 6)"
+
+usage() {
+    echo "
+$(basename "$0") [-s [SOURCE]] [-i [IMG_DESTINATION]] [-v [VID_DESTINATION]] [-t [TAGS]]
+"
+}
 
 while getopts ":s:i:v:t:h" opt; do
     case $opt in
-        h) echo "$(basename "$0") [-s [SOURCE]] [-i [IMG_DESTINATION]] [-v [VID_DESTINATION]] [-t [TAGS]]"
+        h) usage
            exit 0
            ;;
         s) SOURCE="$OPTARG"
@@ -18,14 +25,23 @@ while getopts ":s:i:v:t:h" opt; do
            ;;
         t) TAGS="$OPTARG"
            ;;
-        \?) echo "INVALID OPTION -$OPTARG" >&2
+        \?) echo "${RED}INVALID OPTION -$OPTARG${NC}" >&2
+            usage
+            exit 1
             ;;
     esac
 done
 
 [ -z "$SOURCE" ] && SOURCE="$HOME/Dropbox/Camera Uploads"
+[ -d "$SOURCE" ] || { echo "${RED}$SOURCE doesn't exist. Aborting!${NC}"; exit 1; }
 [ -z "$IMG_DESTINATION" ] && IMG_DESTINATION="$HOME/Dropbox/Pictures"
+[ -d "$IMG_DESTINATION" ] || {
+    echo "${RED}$IMG_DESTINATION doesn't exist. Aborting!${NC}"; exit 1;
+}
 [ -z "$VID_DESTINATION" ] && VID_DESTINATION="$HOME/Dropbox/Videos"
+[ -d "$VID_DESTINATION" ] || {
+    echo "${RED}$VID_DESTINATION doesn't exist. Aborting!${NC}"; exit 1;
+}
 [ -z "$TAGS" ] && TAGS=(
         "createdate"
         "datetimeoriginal"
@@ -37,47 +53,68 @@ FILE_FMT="%Y%m%d.%H%M%S%%c.%%le"
 IMG_DIR_FMT="$IMG_DESTINATION/%Y/%m"
 VID_DIR_FMT="$VID_DESTINATION/%Y/%m"
 IMG_FILE_EXT=(
-    '-ext arw'
-    '-ext bmp'
-    '-ext cr2'
-    '-ext dng'
-    '-ext gif'
-    '-ext heic'
-    '-ext heif'
-    '-ext jpeg'
-    '-ext jpg'
-    '-ext k25'
-    '-ext orf'
-    '-ext nrw'
-    '-ext png'
-    '-ext psd'
-    '-ext raw'
-    '-ext svg'
-    '-ext tif'
-    '-ext tiff'
-    '-ext webp'
+    'arw'
+    'bmp'
+    'cr2'
+    'dng'
+    'eps'
+    'gif'
+    'heic'
+    'heif'
+    'jpeg'
+    'jpg'
+    'k25'
+    'orf'
+    'nef'
+    'nrw'
+    'png'
+    'psd'
+    'raw'
+    'sr2'
+    'svg'
+    'tif'
+    'tiff'
+    'webp'
 )
+for ext in "${IMG_FILE_EXT[@]}"; do
+    IMG_FILE_EXT_OPTS+="-ext $ext "
+done
+for ext in "${IMG_FILE_EXT[@]}"; do
+    if [ "$ext" == "${IMG_FILE_EXT[-1]}" ]; then
+        IMG_FILE_FIND_REGEX+=".*\.$ext"
+    else
+        IMG_FILE_FIND_REGEX+=".*\.$ext\|"
+    fi
+done
+
 VID_FILE_EXT=(
-    '-ext avi'
-    '-ext flv'
-    '-ext m4v'
-    '-ext mkv'
-    '-ext mov'
-    '-ext mp4'
-    '-ext mpeg'
-    '-ext mpg'
-    '-ext mpv'
-    '-ext ogg'
-    '-ext ogv'
-    '-ext vob'
-    '-ext webm'
-    '-ext wmv'
+    'avi'
+    'flv'
+    'm4v'
+    'mkv'
+    'mov'
+    'mp4'
+    'mpeg'
+    'mpg'
+    'mpv'
+    'ogg'
+    'ogv'
+    'vob'
+    'webm'
+    'wmv'
 )
-COMMON_OPTIONS=(
-    -recurse
-    -extractEmbedded
-    -ignoreMinorErrors
-)
+for ext in "${VID_FILE_EXT[@]}"; do
+    VID_FILE_EXT_OPTS+="-ext $ext "
+done
+for ext in "${VID_FILE_EXT[@]}"; do
+    if [ "$ext" == "${VID_FILE_EXT[-1]}" ]; then
+        VID_FILE_FIND_REGEX+=".*\.$ext"
+    else
+        VID_FILE_FIND_REGEX+=".*\.$ext\|"
+    fi
+done
+
+COMMON_OPTS="-recurse -extractEmbedded -ignoreMinorErrors"
 
 for tag in "${TAGS[@]}"; do
     if [ -n "$( find "$SOURCE" -prune -empty 2>/dev/null )" ]; then
@@ -85,14 +122,26 @@ for tag in "${TAGS[@]}"; do
         break
     fi
     echo "${YEL}Renaming all files in $SOURCE to $FILE_FMT using $tag... ${NC}"
-    exiftool "-filename<$tag" -dateFormat "$FILE_FMT" -ext "*" ${COMMON_OPTIONS[*]} "$SOURCE"
+    exiftool "-filename<$tag" -dateFormat "$FILE_FMT" -ext "*" "$COMMON_OPTS" "$SOURCE"
     echo "${YEL}Moving all image files in $SOURCE to $IMG_DIR_FMT using $tag... ${NC}"
-    exiftool "-directory<$tag" -dateFormat "$IMG_DIR_FMT" ${IMG_FILE_EXT[*]} ${COMMON_OPTIONS[*]} "$SOURCE"
+    exiftool "-directory<$tag" -dateFormat "$IMG_DIR_FMT" $IMG_FILE_EXT_OPTS $COMMON_OPTS "$SOURCE"
     echo "${YEL}Moving all video files in $SOURCE to $VID_DIR_FMT using $tag... ${NC}"
-    exiftool "-directory<$tag" -dateFormat "$VID_DIR_FMT" ${VID_FILE_EXT[*]} ${COMMON_OPTIONS[*]} "$SOURCE"
+    exiftool "-directory<$tag" -dateFormat "$VID_DIR_FMT" $VID_FILE_EXT_OPTS $COMMON_OPTS "$SOURCE"
 done
 
-echo "${YEL}Checking $IMG_DESTINATION for video files...${NC}"
-find "$IMG_DESTINATION" -regex '.*\.mp4\|.*\.mov\|.*\.webm\|.*\.m4v\|.*\.mkv'
-echo "${YEL}Checking $VID_DESTINATION for image files...${NC}"
-find "$VID_DESTINATION" -regex '.*\.jpeg\|.*\.jpg\|.*\.png\|.*\.webp\|.*\.gif'
+check_for_inappropriate_files() {
+    local dir="$1" file_regex="$2"
+    printf "${YEL}Checking $dir for image files... ${NC}"
+    readarray -d '' found_files < <(find "$dir" -iregex "$file_regex" -print0)
+    if [ "${#found_files[@]}" == 0 ]; then
+        printf "${CYN}Nothing to see here :-)${NC}\n"
+    else
+        printf "${MAG}Found ${#found_files[@]}:${NC}\n"
+        for f in "${found_files[@]}"; do
+            echo "$f"
+        done
+    fi
+}
+
+check_for_inappropriate_files "$IMG_DESTINATION" "$VID_FILE_FIND_REGEX"
+check_for_inappropriate_files "$VID_DESTINATION" "$IMG_FILE_FIND_REGEX"
